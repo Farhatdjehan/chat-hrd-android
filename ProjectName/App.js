a995eea9114e91fad7b2622d0dc8d304bcafbbf9
farhatdk <farhatdk98@gmail.com>
2022-10-04 14:44:54 +0700

    progress cookies

--- ProjectName/App.js
+++ ProjectName/App.js
@@ -1,41 +1,94 @@
+import CookieManager from '@react-native-cookies/cookies';
 import React, { useEffect, useRef, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import WebView from 'react-native-webview';
+import { Button, Text, TouchableHighlight, TouchableOpacity } from 'react-native';
+
+CookieManager.get('https://chat-hrd.vercel.app')
+  .then((cookies) => {
+    console.log('CookieManager.get =>', cookies, "<==")
+  }
+  );
+
 
 const App = () => {
 
   const [data, setData] = useState();
+  const [dataSendBack, setDataSendBack] = useState();
   const webviewRef = useRef();
 
   useEffect(() => {
-    console.log(data);
+    if (data !== undefined && dataSendBack) {
+      CookieManager.set('https://chat-hrd.vercel.app', {
+        name: 'data',
+        value: `${dataSendBack}`,
+        version: '1',
+        expires: '2029-05-30T12:30:00.00-05:00'
+      }).then((done) => {
+        console.log('CookieManager.set =>', done);
+      });
+    }
+  }, [data, dataSendBack])
+
+  useEffect(() => {
+    if (data) {
+      storeData(data)
+      // sendDataToWebView(data);
+    }
   }, [data])
 
   useEffect(() => {
-    if (data) sendDataToWebView(data);
-  }, [data])
+    getData();
+  }, [])
 
-  const onMessage = (m) => {
-    setData(m?.nativeEvent?.data);
+  useEffect(() => {
+    sendDataToWebView(data);
+  }, [webviewRef])
+
+  const sendDataToWebView = (dataObject) => {
+    webviewRef.current.injectJavaScript(`( function() { 
+      document.dispatchEvent( new MessageEvent('message', { 
+      data: ${JSON.stringify(dataObject)}, 
+      origin : 'react-native' 
+     })); })();` );
+
   }
 
-  function sendDataToWebView(value) {
-    // value =
-    // const injected = value;
-    // webView.current.injectJavaScript(injected);
-    // webviewRef.current.postMessage(value);
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('data')
+      if (value !== null) {
+        setDataSendBack(value);
+        // value previously stored
+      }
+    } catch (e) {
+      console.log(e);
+      // error reading value
+    }
   }
 
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem('data', value)
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-  const message = "webview"
-  let jsCode = 'testfunction(param){ return param }';
+  const onMessage = (e) => {
+    setData(e?.nativeEvent?.data);
+  }
 
   return (
-    <WebView
-      // injectedJavaScript={jsCode}
-      bounces={false}
-      source={{ uri: 'http://192.168.5.130:3000' }}
-      onMessage={onMessage}
-    />
+    <>
+
+      <WebView
+        ref={webviewRef}
+        bounces={false}
+        source={{ uri: 'https://chat-hrd.vercel.app' }}
+        onMessage={onMessage}
+      />
+    </>
   );
 };
 
